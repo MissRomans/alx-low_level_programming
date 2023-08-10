63d4b5e4e901fe8c6063fab2954e0e8277068953
MissRomans <etiquettemusic184@gmail.com>
2023-08-10 05:47:55 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - program that prints output with the function put
+ *
+ * Return: Always 0
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return(0);
+}
