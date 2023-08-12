1ddbe303bca7b4d30eb42b873310c072aca95f6a
MissRomans <etiquettemusic184@gmail.com>
2023-08-12 01:54:03 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char m;
+
+	for (m = 0 ; m <= 10 ; m++)
+	{
+		putchar(m);
+	}
+	putchar ('\n');
+	return (0);
+}
