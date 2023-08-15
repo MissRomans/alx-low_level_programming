73bc6c2f7ddd15930626ab87c85a771b180d9658
MissRomans <etiquettemusic184@gmail.com>
2023-08-15 05:03:10 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that prints the alphabet, in lowercase
+ *
+ * Return:void
+ */
+
+void print_alphabet(void)
+{
+	char m;
+
+	for (m = 'a'; m <= 'z'; m++)
+	_putchar(m);
+
+	_putchar('\n');
+}
