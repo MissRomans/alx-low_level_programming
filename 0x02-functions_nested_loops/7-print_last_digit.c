a90f24234eb90ef063e56647a5aa382895ffb69e
MissRomans <etiquettemusic184@gmail.com>
2023-08-15 10:58:19 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,11 +10,11 @@ int print_last_digit(int m)
 {
 	int last_digit;
 
-	if (m < 0)
-	{
 	last_digit = m % 10;
-
+	if (last_digit < 0)
+	{
 	last_digit = last_digit * -1;
 	}
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
