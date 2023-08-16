c20bd1c7777fcbaf3449d8fb059f4be8baafb66e
MissRomans <etiquettemusic184@gmail.com>
2023-08-15 23:43:08 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns the result
+ * @m: First number to be added
+ * @n: Second number to be added
+ * Return: Integer result
+ */
+
+int add(int m, int n)
+{
+	return (m + n);
+}
