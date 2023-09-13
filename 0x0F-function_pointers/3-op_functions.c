3b91784c5cb91ad7dee0d384ee253ca0ce443392
MissRomans <etiquettemusic184@gmail.com>
2023-09-13 09:00:47 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+/**
+ * op_add - returns the sum of a and b
+ * @a: first number
+ * @b: second number
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - returns the difference of a and b
+ * @a: first number
+ * @b:second number
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - returns the result of the division
+ * @a: first number
+ * @b: second number
+ * Return: division of the result of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - returns the result of the division of a by b
+ * @a: first number
+ * @b: second number
+ * Return: result of division of a by b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - returns the remainder of the division of a by b
+ * @a: first number
+ * @b: second number
+ * Return: remainder of division of a by b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
