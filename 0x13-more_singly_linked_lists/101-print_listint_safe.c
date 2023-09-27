f56332f0fbbf0bd876edc6b2c8d6969a02a9c13b
MissRomans <etiquettemusic184@gmail.com>
2023-09-27 00:25:36 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,33 +2,77 @@
 #include <stdio.h>
 
 /**
- * Prints a listint_t linked list
+ * looped_listint_len - Counts the number of unique nodes
+ * in a looped listint_t linked list
+ * @head: pointer to the head of the listsint_t
+ * Return: if the list is not looped, return 0
+ */
+
+size_t looped_listint_len(const listint_t *head)
+{
+	size_t nodes = 0;
+	const listint_t *title, *name;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	title = head->next;
+	name = (head->next)->next;
+
+	while (name)
+	{
+		if (title == name)
+		{
+			title = head;
+			while (title != name)
+			{
+				nodes++;
+				title = title->next;
+				name = name->next;
+			}
+			title = title->next;
+			while (title != name)
+			{
+				nodes++;
+				title = title->next;
+			}
+			return (nodes);
+		}
+		title = title->next;
+		name = (name->next)->next;
+	}
+	return (0);
+}
+
+/**
+ * print_listint_safe - Prints a listint_t linked list
  * @head: pointer to the head of the listsint_t
  * Return: the number of nodes in the list
  */
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes = 0;
-	const_listint_t *tmp, *name, *new_node;
+	size_t nodes, index = 0;
 
-	tmp = NULL;
-	while (head != NULL)
+	nodes = looped_listint_len(head);
+	if (nodes == 0)
 	{
-		name = head;
-
-		while (name != tmp)
+		for (; head != NULL; nodes++)
 		{
-			if (name == tmp->next)
-			{
-				printf("-> [%p] %d\n", (void *)tmp, tmp->n);
-				return (nodes);
-			}
-			name = name->next;
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
-		printf("[%p] %d\n", (void *)tmp, tmp->n);
-		tmp = tmp->next;
-		nodes++;
+	}
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+		printf("->[%p] %d\n", (void *)head, head->n);
 	}
 	return (nodes);
 }
+
+
