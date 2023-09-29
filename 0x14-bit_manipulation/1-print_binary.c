4e3dec4b8305b5454750a03476ce46008d5a4405
MissRomans <etiquettemusic184@gmail.com>
2023-09-29 06:30:26 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,7 +7,22 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned int num;
+	unsigned long int num;
 	int m, count = 0;
 
-	for (m = 0; 
+	for (m = 63; m >= 0; m--)
+	{
+		num = n >> m;
+		if (num & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (num)
+		{
+			_putchar('0');
+		}
+	}
+	if (!num)
+		_putchar ('0');
+}
